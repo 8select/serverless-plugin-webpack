d6ea4ec3d9eef3b742c507132cc2a2b50d63fc43
Robert Schweizer <mail@rschweizer.me>
2017-02-01 15:38:04 +0100

    Initial version
    Load external configuration and bundle on serverless deploy initialize lifecycle event

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+
+class ServerlessPluginWebpack {
+  constructor (serverless, options) {
+    this.serverless = serverless
+    this.options = options
+
+    this.commands = {
+      deploy: {
+        lifecycleEvents: [
+          'initialize'
+        ]
+      }
+    }
+
+    this.hooks = {
+      'before:deploy:initialize': this.bundle.bind(this)
+    }
+  }
+
+  bundle () {
+    this.serverless.cli.log('Bundling service...')
+    return new Promise((resolve, reject) => {
+      const config = require(path.join(this.serverless.config.servicePath, 'webpack.config.js'))
+      webpack(config, (error, stats) => {
+        if (error) {
+          reject(error)
+        }
+        resolve()
+      })
+    })
+  }
+}
+
+module.exports = ServerlessPluginWebpack
